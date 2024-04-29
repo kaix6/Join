349faa01af62343620d0feb8342d7fef32d9e958
Isabell Diterle <160329592+Tr0vat@users.noreply.github.com>
2024-04-29 17:07:18 +0200

    “render-tasks-functions”

--- js/board.js
+++ js/board.js
@@ -1,3 +1,5 @@
+let currentDraggedTask;
+
 function showDialogTask(){
     document.querySelector('.background-big-task').classList.toggle('d-none');
     setTimeout(function() {
@@ -9,3 +11,84 @@ function closeDialogTask(){
     document.querySelector('.background-big-task').classList.add('d-none');
     document.querySelector('.task-box-big').classList.remove('show-task-box-big');
 }
+
+// um alle Daten zu laden
+// function init(){ 
+//     loadUsers
+//     loadTasks
+// }
+
+// um Daten zu speichern - potenziell in der allgemeinen script.js?
+// function setItem(){
+// }
+
+// um Daten zu laden - potenziell in der allgemeinen script.js?
+// function getItem(){
+// }
+
+// Diese Funktion rendert alle Tasks je Spalte auf dem Board
+function updateTasksHTML(){
+    let tasks = getTasksJson();
+    updateOpenTasks(tasks);
+    updateInProgressTasks(tasks);
+    updateAwaitFeedbackTasks(tasks);
+    updateDoneTasks(tasks);
+}
+
+// Diese Funktion holt das JSON Array mit den Tasks und gibt es an die update-Funktionen weiter
+async function getTasksJson(){
+    let response = await fetch('./js/addTasks.json');
+    tasks = await response.json();
+    return tasks;
+}
+
+// Diese Funktion rendert die Tasks mit dem Status "open"
+function updateOpenTasks(tasks){
+    let open = tasks.filter(t => t['status'] == 'open')
+    let boardOpenTasks = document.getElementById('openTasks');
+
+    if(open.length > 0){
+        boardOpenTasks.innerHTML = '';
+        for (let i = 0; i < open.length; i++) {
+            const openTask = open[i];
+            boardOpenTasks.innerHTML += generateSmallTaskBox(openTask);
+        };
+    } else {
+        boardOpenTasks.innerHTML = '';
+        boardOpenTasks.innerHTML = noTasksBox();
+    };
+}
+
+// Diese Funktion rendert die Tasks mit dem Status "in Progress"
+function updateInProgressTasks(tasks){
+    let inProgress = tasks.filter(t => t['status'] == 'in progress');
+    let boardInProgressTasks = document.getElementById('inProgressTasks');
+    boardInProgressTasks.innerHTML = '';
+    for (let i = 0; i < inProgress.length; i++) {
+        const inProgressTask = inProgress[i];
+        boardInProgressTasks.innerHTML += generateSmallTaskBox(inProgressTask);
+    };
+}
+
+// Diese Funktion rendert die Tasks mit dem Status "await Feedback"
+function updateAwaitFeedbackTasks(tasks){
+    let awaitFeedback = tasks.filter(t => t['status'] == 'await feedback');
+    let boardAwaitFeedbackTasks = document.getElementById('awaitFeedbackTasks');
+    boardAwaitFeedbackTasks.innerHTML = '';
+    for (let i = 0; i < awaitFeedback.length; i++) {
+        const awaitFeedbackTask = awaitFeedback[i];
+        boardAwaitFeedbackTasks.innerHTML += generateSmallTaskBox(awaitFeedbackTask);
+    };
+}
+
+// Diese Funktion rendert die Tasks mit dem Status "done"
+function updateDoneTasks(tasks){
+    let done = tasks.filter(t => t['status'] == 'done');
+    let boardDoneTasks = document.getElementById('doneTasks');
+    boardDoneTasks.innerHTML = '';
+    for (let i = 0; i < done.length; i++) {
+        const doneTask = done[i];
+        boardDoneTasks.innerHTML += generateSmallTaskBox(doneTask);
+    };
+}
+
--- js/template.js
+++ js/template.js
@@ -33,6 +33,36 @@ function hideHelpIcon() {
     } 
 }
 
+function smallTaskBox(){
+    return `<div>
+        <div id="task-box-small" class="task-box" onclick="showDialogTask()">
+            <p class="task-type">Task Type</p>
+            <p class="task-headline">Task Headline</p>
+            <p class="task-description">Task Description</p>
+            <div class="progress-section">
+                <div class="progress">
+                    <div class="progress-bar" style="width: 50%;"></div>
+                </div>
+                <p class="amount-subtask">1/2 Subtasks</p>
+            </div>
+            <div class="member-prio-section">
+                <div class="task-all-member">
+                    <div class="member">AM</div>
+                    <div class="member">EM</div>
+                </div>
+                <img src="/assets/img/add_task/prio_low.svg">
+            </div>
+        </div>
+    </div>`;
+}
+
+function generateSmallTaskBox(){
+    return `
+    <div class="no-task-div">
+        <p>No tasks To do</p>
+    </div>`;
+}
+
 
 
 
