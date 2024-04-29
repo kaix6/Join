c275104c3474529c7ca4d07ae704489f2747db60
Jenanie87 <107427021+Jenanie87@users.noreply.github.com>
2024-04-29 13:01:00 +0200

    “added-function-render-contacts”

--- js/contacts.js
+++ js/contacts.js
@@ -9,6 +9,72 @@ let colors = ['var(--tagOrange)', 'var(--tagPink)', 'var(--tagPurple)',
 ]
 
 
+async function initJSONContacts() {
+    let response = await fetch('./js/contacts.json');
+    contacts = await response.json();
+}
+
+
+
+async function loadContacts() {
+    await initJSONContacts();
+    let contentContacts = document.querySelector('.contacts');
+    let sortedCcontacts = sortArray(contacts);
+    contentContacts.innerHTML = '';
+
+
+    for (let i = 0; i < sortedCcontacts.length; i++) {
+        const contact = sortedCcontacts[i];
+        contentContacts.innerHTML += generateContactsInnerHTML(contact, i);
+        changeColorContact(i, contact.color);
+        
+    }
+    
+}
+
+
+function generateContactsInnerHTML(contact, i) {
+    return /* HTML */ `
+        <div class="letter_content">
+            <div class="letter_names">
+                <p class="head_letters">A</p>
+            </div>
+            <div class="horizontal_line_content">
+                <div class="horizontal_line"></div>
+            </div>
+                <div onclick="toggleContactView()" id="contact${i}" class="contact pointer"> <!-- Vergabe einer ID für JavaScript, wenn in JS gerendert wird -->
+                    <div id="short_name${i}" class="short_name round_div">
+                        <p class="short_name_text">${contact.letters}</p>
+                    </div>
+                    <div class="name_data flex_dir_c">
+                        <p class="contact_fullName">${contact.name}</p>
+                        <p class="contact_mail">${contact.mail}</p>
+                    </div>
+                </div>
+        </div>`;
+}
+
+
+function changeColorContact(i, color) {
+    let shortName = document.querySelector(`#short_name${i}`);
+    shortName.style.backgroundColor = color;
+}
+
+
+function sortArray(array) {
+    let sortedArray = array.sort((a, b) => {
+        if(a.name < b.name) {
+            return -1;
+        }
+        if(a.name > b.name) {
+            return 1;
+        }
+        return 0; // Wenn die Namen gleich sind
+    })
+    return sortedArray;
+}
+
+
 function toggleContactView() {
     // Mobile
     if(currentElementWidth(1110)) {
@@ -48,17 +114,6 @@ function getRandomItem(array) {
     return item;
 }
 
-
-async function initJSONContacts() {
-    let response = await fetch('./js/contacts.json');
-    contacts = await response.json();
-
-    for (let i = 0; i < contacts.length; i++) {
-        const element = contacts[i];
-        let initials = getContactsInitials(element.name);
-        return initials;
-    }
-}
 
 // Evtl. auch die Anfangsbuchstaben im json array übernhemen und die unten stehende Funktion nur beim Neuanlegen oder editieren anwenden wie auch bei color : getRandomItem(colors);
 
