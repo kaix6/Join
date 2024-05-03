750e4da9831f5b306d36fedf46e9c153e71659d9
seidenberger <158757732+seidenberger@users.noreply.github.com>
2024-05-03 11:02:50 +0200

    register_login

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function login() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(u => u.email == email.value && u.password == password.value);
+    console.log(user);
+    if (user) {
+        console.log('user gefunden')
+    }
+    window.location.href = '/summary.html?';
+}
--- js/register.js
+++ js/register.js
@@ -1,7 +1,14 @@
 function addUser() {
+    let name = document.getElementById('name');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    users.push({ email: email.value, password: password.value });
-    window.location.href = '/loginindex.html';
+
+    users.push({ email: email.value, password: password.value, name: name.value });
+
+    // ändern auf backen speicherung
+    let allUserAsString = JSON.stringify(users);
+    localStorage.setItem('users', allUserAsString);
+
+    window.location.href = '/index.html';
 
 }
