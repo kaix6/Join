6f11ef0c24ad758213288d138494047683473d49
Jenanie87 <107427021+Jenanie87@users.noreply.github.com>
2024-05-04 11:53:56 +0200

    “move-generateInnerHTML-to-new-file”

--- js/generateHTML.js
+++ js/generateHTML.js
@@ -38,3 +38,123 @@ function generateNoTaskBox(){
         <p>No tasks To do</p>
     </div>`;
 }
+
+
+// Contacts
+
+function generateContactsInnerHTML(contact, i) {
+    return /* HTML */ `
+        <div class="letter_content">
+            <div id="letter_names${i}" class="letter_names">
+<!--                 <p class="head_letters">A</p> -->
+            </div>
+            <div class="horizontal_line_content">
+                <div class="horizontal_line"></div>
+            </div>
+                <div onclick="toggleContactView(${i})" id="contact${i}" class="contact pointer"> <!-- Vergabe einer ID für JavaScript, wenn in JS gerendert wird -->
+                    <div id="short_name${i}" class="short_name round_div">
+                        <p class="short_name_text">${contact.letters}</p>
+                    </div>
+                    <div class="name_data flex_dir_c">
+                        <p class="contact_fullName">${contact.name}</p>
+                        <p class="contact_mail">${contact.mail}</p>
+                    </div>
+                </div>
+        </div>`;
+}
+
+
+function generateLettersInnerHTML(letter) {
+    return /* HTML */ `
+        <p class="head_letters">${letter}</p>`;
+}
+
+
+function generateFloatingContactInnerHTML(i) {
+    return /* HTML */ `
+        <div onclick="showContactOptions(${i})" class="add_change_btn_mobile hide_desktop pointer">
+            <img class="add_person_more_icon" src="assets/img/contacts/more_vert.svg" alt="add person icon">
+        </div>
+        <div class="head_floating_content">
+            <div id="short_name_overview${i}" class="short_name_overview round_div">
+                <p class="short_name_text_overview">${sortedContacts[i].letters}</p>
+            </div>
+        <div class="name_editable_content">
+            <h3 class="name_overview">${sortedContacts[i].name}</h3>
+            <div class="editable_content">
+                <div onclick="editContact(event, ${i})" class="edit_content pointer">
+                    <img class="contact_edit_icon img_width24" src="assets/img/contacts/edit.svg" alt="edit icon">
+                    <p>Edit</p>
+                </div>
+                <div onclick="deleteContact(${i})" class="delete_content pointer">
+                    <img class="contact_delete_icon img_width24" src="assets/img/contacts/delete.svg" alt="delete icon">
+                    <p>Delete</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        <p class="text_information">Contact Information</p>
+        <div class="contact_information">
+            <div class="contact_overview_mail">
+                <h4>Email</h4>
+                <p class="overview_mail">${sortedContacts[i].mail}</p>
+            </div>
+            <div class="contact_overview_phone">
+                <h4>Phone</h4>
+                <p>${sortedContacts[i].phone}</p>
+            </div>
+        </div>`;
+}
+
+
+function generateContactOptionsInnerHTML(i) {
+    return /*HTML*/ `
+        <div onclick="editContact(event, ${i})" class="edit_content pointer">
+            <img class="contact_edit_icon img_width24" src="assets/img/contacts/edit.svg" alt="edit icon">
+            <p>Edit</p>
+        </div>
+        <div onclick="deleteContact(${i})" class="delete_content pointer">
+            <img class="contact_delete_icon img_width24" src="assets/img/contacts/delete.svg" alt="delete icon">
+            <p>Delete</p>
+        </div> `;
+}
+
+
+function generateDialoEditInnerHTML(index) {
+    return /* HTML */`
+        <div onclick="doNotClose(event)" class="dialog_edit_contact">
+            <div onclick="closeDialog('.dialog_edit_contact', 'show_dialog_edit_contact', '.dialog_edit_contact_bg', 'd_none', 100)" class="wrapper_close_add_edit_contact round_div pointer">
+                <img class="close_add_edit_contact pointer" src="./assets/img/general/close.svg" alt="close icon">
+            </div>
+        <div class="top_dialog_add_edit flex_dir_c">
+            <div class="head_top_dialog_add_edit flex_dir_c">
+                <img class="img_logo" src="./assets/img/general/logo.svg" alt="join logo">
+                <h3 class="headline_add_edit_contact">Edit contact</h3>
+                <div class="seperator_line_content_contact">
+                </div>
+            </div>
+        </div>
+        <div id="create_contact_short_name_edit${index}" class="create_contact_short_name_edit round_div">
+            <p class="short_name_text_overview_edit">${sortedContacts[index].letters}</p>
+        </div>
+        <div class="bottom_dialog_add_edit">
+            <form onsubmit="return false;" class="create_contact_form">
+                <label for="fullName_edit"></label>
+                <input class="create_contact_input" min="2" type="text" id="fullName_edit" placeholder="Name" required>
+                <label for="mail_edit"></label>
+                <input class="create_contact_input" type="email" id="mail_edit" placeholder="Email" required>
+                <label for="telNumber_edit"></label>
+                <input class="create_contact_input" type="tel" id="telNumber_edit" placeholder="Phone" required>
+                <div class="container_button">
+                    <button onclick="saveNewData(${index})" class="cancel_create_contact_edit_btn contact_btn pointer">
+                        <p class="text_cancel_create_contact_edit_btn">Delete</p>
+                    </button>
+                    <button class="create_contact_btn contact_btn pointer">
+                        <p class="text_create_contact_btn">Save</p>
+                        <img class="img_checked_btn img_width24" src="./assets/img/contacts/check.svg" alt="checked icon">
+                    </button>
+                </div>
+            </form>
+        </div>
+        </div>`;
+}
