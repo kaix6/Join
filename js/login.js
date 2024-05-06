2af92c787d5aaf97fc5c49f627a0b88cb30a3888
seidenberger <158757732+seidenberger@users.noreply.github.com>
2024-05-06 18:02:24 +0200

    index_login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,8 @@
+async function addUsersJason() {
+    let response = await fetch('./js/users.json');
+    users = await response.json();
+}
+
 /**
  * This function is to animat the start page
  */
@@ -12,10 +17,11 @@ function animationWindow() {
  * This function is to checkt for the rigth password 
  */
 
-function login() {
+async function login() {
+    await addUsersJason();
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    let user = users.find(u => u.email == email.value && u.password == password.value);
+    let user = users.find(u => u.mail == email.value && u.password == password.value);
     console.log(user);
     if (user) {
         console.log('user gefunden')
