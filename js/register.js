6c30d926c4e1b849d588dfac0407e0bc59022a51
seidenberger <158757732+seidenberger@users.noreply.github.com>
2024-05-16 22:25:20 +0200

    register

--- js/register.js
+++ js/register.js
@@ -7,13 +7,23 @@ function addUser() {
     let name = document.getElementById('name');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-
+    matchPassword();
     users.push({ name: name.value, email: email.value, password: password.value, confirmPassword: confirmPassword.value });
 
     // ändern auf backen speicherung
-    let allUserAsString = JSON.stringify(users);
-    localStorage.setItem('users', allUserAsString);
+    // let allUserAsString = JSON.stringify(users);
+    // localStorage.setItem('users', allUserAsString);
 
     window.location.href = './index.html';
 
+}
+
+function matchPassword() {
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    if (password != confirmPassword) {
+        alert("Passwords did not match");
+    } else {
+        alert("Password created successfully");
+    }
 }
