4e8ce4ebe3a1749e727f372c94db7cecccd2dc19
Kai Janicek <100490095+kaix6@users.noreply.github.com>
2024-05-20 14:29:43 +0200

    “JSDoc-addTask”

--- js/add_tasks.js
+++ js/add_tasks.js
@@ -10,6 +10,8 @@ let allTasksJson = [];
 
 /**
  * Changes the highlighting color and appearance of the priority button based on the provided priority.
+ * The function prevents the default behavior of the event, clears existing classes from the priority buttons and their images,
+ * and then applies new classes to highlight the selected priority.
  * @param {string} prio - The priority of the button ("urgent", "medium", or "low").
  * @param {Event} event - The event object that triggered the call.
  * @returns {void}
@@ -22,33 +24,26 @@ function addPrioButtonColor(prio, event) {
   let imgUrgent = document.getElementById("buttonImg1");
   let imgMedium = document.getElementById("buttonImg2");
   let imgLow = document.getElementById("buttonImg3");
-  removeClasses(
-    buttonUrgent,
-    buttonMedium,
-    buttonLow,
-    imgUrgent,
-    imgMedium,
-    imgLow
-  ); // Remove existing classes to clear previous highlights
+  removeClasses(buttonUrgent, buttonMedium, buttonLow, imgUrgent, imgMedium, imgLow); // Remove existing classes to clear previous highlights
   if (prio === "urgent") {
-    selectedButtonColor(
-      buttonUrgent,
-      imgUrgent,
-      "backgroundColorRed",
-      "urgent"
-    );
+    selectedButtonColor(buttonUrgent, imgUrgent, "backgroundColorRed", "urgent");
   } else if (prio === "medium") {
-    selectedButtonColor(
-      buttonMedium,
-      imgMedium,
-      "backgroundColorOrange",
-      "medium"
-    );
+    selectedButtonColor(buttonMedium, imgMedium, "backgroundColorOrange", "medium");
   } else if (prio === "low") {
     selectedButtonColor(buttonLow, imgLow, "backgroundColorGreen", "low");
   }
 }
 
+/**
+ * Applies the specified background color and appearance styles to a priority button and its associated image.
+ * This function adds CSS classes to the button and image elements to change their appearance
+ * according to the specified priority and updates the global `selectedPrio` variable.
+ * @param {HTMLElement} button - The button element to be styled.
+ * @param {HTMLElement} img - The image element associated with the button.
+ * @param {string} backgroundColorClass - The CSS class for the background color to be applied to the button.
+ * @param {string} prio - The priority value ("urgent", "medium", or "low") to be assigned.
+ * @returns {void}
+ */
 function selectedButtonColor(button, img, backgroundColorClass, prio) {
   button.classList.add(backgroundColorClass, "fontWeightAndColor");
   img.classList.add("imgColor");
@@ -57,6 +52,8 @@ function selectedButtonColor(button, img, backgroundColorClass, prio) {
 
 /**
  * Initializes the process of adding tasks.
+ * This function sets up the necessary components for the task addition process by including HTML content,
+ * rendering the contacts in the add tasks section, and initializing the JSON data needed for adding tasks.
  * @returns {void}
  */
 function initAddTasks() {
@@ -67,6 +64,8 @@ function initAddTasks() {
 
 /**
  * Removes the highlighting classes from priority buttons and their images.
+ * This function clears specific CSS classes from the provided button and image elements to remove any previously applied
+ * highlighting styles. The classes removed include those used for background colors, font weight, and image color.
  * @param {HTMLElement} buttonUrgent - The urgent priority button element.
  * @param {HTMLElement} buttonMedium - The medium priority button element.
  * @param {HTMLElement} buttonLow - The low priority button element.
@@ -75,29 +74,9 @@ function initAddTasks() {
  * @param {HTMLElement} imgLow - The image associated with the low priority button.
  * @returns {void}
  */
-function removeClasses(
-  buttonUrgent,
-  buttonMedium,
-  buttonLow,
-  imgUrgent,
-  imgMedium,
-  imgLow
-) {
-  const classesToRemove = [
-    "backgroundColorRed",
-    "fontWeightAndColor",
-    "backgroundColorOrange",
-    "backgroundColorGreen",
-    "imgColor",
-  ];
-  const elements = [
-    buttonUrgent,
-    buttonMedium,
-    buttonLow,
-    imgUrgent,
-    imgMedium,
-    imgLow,
-  ];
+function removeClasses(buttonUrgent, buttonMedium, buttonLow, imgUrgent, imgMedium, imgLow) {
+  const classesToRemove = ["backgroundColorRed", "fontWeightAndColor", "backgroundColorOrange", "backgroundColorGreen", "imgColor",];
+  const elements = [buttonUrgent, buttonMedium, buttonLow, imgUrgent, imgMedium, imgLow];
 
   for (let i = 0; i < elements.length; i++) {
     let element = elements[i];
@@ -116,7 +95,12 @@ function reloadPage() {
   location.reload();
 }
 
-// Funktion zum Rendern von Kontakten in das Feld "Assigned To"
+/**
+ * Renders contacts into the "Assigned To" field.
+ * This function loads contacts, populates the "Assigned To" dropdown, and sets up an event listener
+ * for changes in the dropdown.
+ * @returns {Promise<void>}
+ */
 async function renderContactsInAddTasks() {
   const contacts = Object.entries(await loadData("contacts"));
   const assignedTo = document.getElementById("assignedTo");
@@ -124,7 +108,7 @@ async function renderContactsInAddTasks() {
   assignedTo.innerHTML = generateAssignedToFirst(); // Platzhalter
 
   contacts.forEach((contact) => {
-    const {name, letters} = contact[1];
+    const { name, letters } = contact[1];
     assignedTo.innerHTML += `<option value="${name}">${name}</option>`;
   });
 
@@ -132,7 +116,15 @@ async function renderContactsInAddTasks() {
     handleAssignedToChange(contacts, assignedTo, MembersArea);
 }
 
-// Funktion zum Bearbeiten der Auswahländerung
+/**
+ * Handles the change event for the "Assigned To" selection.
+ * This function updates the selected members area based on the selected contact
+ * and adds the selected contact to the members list.
+ * @param {Array} contacts - The list of contacts.
+ * @param {HTMLSelectElement} assignedTo - The "Assigned To" dropdown element.
+ * @param {HTMLElement} MembersArea - The area where selected members are displayed.
+ * @returns {void}
+ */
 function handleAssignedToChange(contacts, assignedTo, MembersArea) {
   const selectedName = assignedTo.value;
   const selectedContact = contacts.find(
@@ -147,7 +139,16 @@ function handleAssignedToChange(contacts, assignedTo, MembersArea) {
   }
 }
 
-// Funktion zum Hinzufügen der ausgewählten Benutzer (Anzeigen von Profilbildern) unter "Assigned To"
+/**
+ * Adds the selected user to the "Assigned To" list and displays their profile image.
+ * This function checks if the user is already selected, shows an appropriate message,
+ * and if not, adds the user's details to the selected users list and updates the display.
+ * @param {Object} contact - The contact object containing user details.
+ * @param {string} contact.name - The name of the contact.
+ * @param {string} contact.color - The color associated with the contact.
+ * @param {string} contact.letters - The letters representing the contact.
+ * @returns {void}
+ */
 function pushMembers(contact) {
   const { name, color, letters } = contact;
   const messageSelected = document.getElementById("isSelected");
@@ -157,14 +158,19 @@ function pushMembers(contact) {
     return;
   }
   messageIsSelected(messageSelected);
-
-  // Füge das Element und seine Farbe zu selectUsers und selectUsersColor hinzu
   selectUsers.push(name);
   selectUsersColor.push(color);
   selectUsersLetters.push(letters);
   renderPushedMembers();
 }
 
+/**
+ * Displays a message indicating that a user has been selected.
+ * This function updates the visibility of the provided message element
+ * to indicate that a user has been successfully selected.
+ * @param {HTMLElement} messageSelected - The message element to update.
+ * @returns {void}
+ */
 function messageIsSelected(messageSelected) {
   messageSelected.classList.remove("unset-display");
   messageSelected.classList.add("none-display");
@@ -175,7 +181,12 @@ function messageMemberIsAdded(messageSelected) {
   messageSelected.classList.remove("none-display");
 }
 
-// Renders the selected users below the "Assigned To" section
+/**
+ * Renders the selected users below the "Assigned To" section.
+ * This function clears the existing content in the selected members area
+ * and then iterates over the selected users list to display their profile images.
+ * @returns {void}
+ */
 function renderPushedMembers() {
   const selectMembersArea = document.getElementById("selectedMembers");
   selectMembersArea.innerHTML = "";
@@ -186,21 +197,26 @@ function renderPushedMembers() {
     const letters = selectUsersLetters[i];
 
     if (!document.getElementById(`${element}`)) {
-    selectMembersArea.innerHTML += `<div id="${element}" class="profilbild">${letters}</div>`;
-    document.getElementById(`${element}`).style.backgroundColor = `${color}`;
+      selectMembersArea.innerHTML += `<div id="${element}" class="profilbild">${letters}</div>`;
+      document.getElementById(`${element}`).style.backgroundColor = `${color}`;
     }
   }
 }
 
-// Funktion zum rendern in das addTasks.json
-async function saveTaskToJson(
-  title,
-  description,
-  date,
-  prio,
-  category,
-  assignedTo
-) {
+/**
+ * Renders the task details into the addTasks.json file.
+ * This function creates a new task object with the provided details,
+ * adds it to the tasks data, and then logs a success message.
+ * Finally, it displays the dialog for adding the task to the board.
+ * @param {string} title - The title of the task.
+ * @param {string} description - The description of the task.
+ * @param {string} date - The due date of the task.
+ * @param {string} prio - The priority of the task ("urgent", "medium", or "low").
+ * @param {string} category - The category of the task.
+ * @param {string} assignedTo - The member assigned to the task.
+ * @returns {void}
+ */
+async function saveTaskToJson(title, description, date, prio, category, assignedTo) {
   // Neuen Task erstellen
   let newTask = {
     title: title,
@@ -212,24 +228,34 @@ async function saveTaskToJson(
     subtask: subtaskArray,
     status: "open",
   };
-
   await postData("tasks", newTask);
-
   console.log("Task erfolgreich hinzugefügt.");
   showAddToBoardDialog();
 }
 
+/**
+ * Displays the add to board dialog box.
+ * This function shows the dialog box for adding a task to the board
+ * the "none-display" class after 1500 milliseconds.
+ * @returns {void}
+ */
 function showAddToBoardDialog() {
-  let dialogbox = document.getElementById('dialogboxTask');
-  dialogbox.classList.remove('none-display'); 
-  dialogbox.classList.add('show_dialog_addTask'); 
-  setTimeout(function() {
-    dialogbox.classList.remove('show_dialog_addTask');
-    dialogbox.classList.add('none-display');
-}, 1500)
+  let dialogbox = document.getElementById("dialogboxTask");
+  dialogbox.classList.remove("none-display");
+  dialogbox.classList.add("show_dialog_addTask");
+  setTimeout(function () {
+    dialogbox.classList.remove("show_dialog_addTask");
+    dialogbox.classList.add("none-display");
+  }, 1500);
 }
 
-// Funktion für hinzufügen neuer Subtasks
+/**
+ * Adds a new subtask.
+ * This function retrieves the value of the subtask input field, validates it,
+ * generates a unique ID for the new subtask, and then calls another function to generate
+ * the HTML for the subtask and append it to the subtask area.
+ * @returns {void}
+ */
 function addNewSubtask() {
   let subtaskValue = document.getElementById("subtask").value;
   let subtaskText = document.getElementById("textSubtask");
@@ -243,12 +269,18 @@ function addNewSubtask() {
   generateSubtaskArea(subtaskText, subtaskArea, subtaskValue, subtaskId);
 }
 
-function generateSubtaskArea(
-  subtaskText,
-  subtaskArea,
-  subtaskValue,
-  subtaskId
-) {
+/**
+ * Generates and displays a new subtask area.
+ * Updates subtask text visibility, generates HTML for the new subtask,
+ * appends it to the subtask area, updates area visibility, clears input field,
+ * and adds subtask value to an array.
+ * @param {HTMLElement} subtaskText - Element for displaying subtask-related messages.
+ * @param {HTMLElement} subtaskArea - Area for displaying subtasks.
+ * @param {string} subtaskValue - Value of the new subtask.
+ * @param {string} subtaskId - Unique ID for the new subtask.
+ * @returns {void}
+ */
+function generateSubtaskArea(subtaskText, subtaskArea, subtaskValue, subtaskId) {
   subtaskText.classList.remove("unset-display");
   subtaskArea.innerHTML += generateSubtaskInnerHTML(subtaskId, subtaskValue);
   subtaskArea.classList.remove("subtaskAreaNone");
@@ -256,6 +288,13 @@ function generateSubtaskArea(
   subtaskArray.push(subtaskValue);
 }
 
+/**
+ * Removes a subtask element.
+ * This function removes the subtask element with the specified ID from the DOM,
+ * logging a success message if the element is found.
+ * @param {string} subtaskId - The ID of the subtask element to remove.
+ * @returns {void}
+ */
 function removeSubtask(subtaskId) {
   let subtaskToRemove = document.getElementById(subtaskId);
   if (subtaskToRemove) {
@@ -266,7 +305,13 @@ function removeSubtask(subtaskId) {
   }
 }
 
-// Funktion zum Bearbeiten eines bestimmten Subtasks
+/**
+ * Edits a specific subtask.
+ * This function retrieves the subtask element with the specified ID from the DOM,
+ * creates an input field for editing, and sets up event listeners for editing and saving.
+ * @param {string} subtaskId - The ID of the subtask element to edit.
+ * @returns {void}
+ */
 function editSubtask(subtaskId) {
   let subtaskElement = document.getElementById(subtaskId);
   if (subtaskElement) {
@@ -282,6 +327,15 @@ function editSubtask(subtaskId) {
   }
 }
 
+/**
+ * Sets up event listener to confirm changes with Enter key.
+ * This function adds an event listener to the edit input field to detect when
+ * the Enter key is pressed. If Enter is pressed and the input is not empty,
+ * it updates the subtask text element with the edited value.
+ * @param {HTMLInputElement} editInput - The input field for editing.
+ * @param {HTMLElement} subtaskTextElement - The element displaying the subtask text.
+ * @returns {void}
+ */
 function acceptEnter(editInput, subtaskTextElement) {
   editInput.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
@@ -293,18 +347,35 @@ function acceptEnter(editInput, subtaskTextElement) {
   });
 }
 
+/**
+ * Renders an input field for editing a subtask.
+ * Configures the provided input field for editing, sets its type to "text",
+ * initializes its value with the current subtask text, adds a styling class,
+ * replaces the subtask text element with the input field, and sets focus to it.
+ * @param {string} subtaskText - The current text of the subtask.
+ * @param {HTMLElement} subtaskTextElement - The element displaying the subtask text.
+ * @param {HTMLInputElement} editInput - The input field for editing.
+ * @returns {void}
+ */
 function renderEditInputField(subtaskText, subtaskTextElement, editInput) {
   editInput.type = "text";
-  editInput.value = subtaskText; // Setze den aktuellen Text als Standardwert im Eingabefeld
+  editInput.value = subtaskText; // Set current text as default value
   editInput.classList.add("editInput");
-  // Ersetze den Text durch das Eingabefeld
-  subtaskTextElement.innerHTML = ""; // Entferne den bestehenden Text
-  subtaskTextElement.appendChild(editInput); // Füge das Eingabefeld hinzu
+   // Replace text with input field
+  subtaskTextElement.innerHTML = ""; // Remove existing text
+  subtaskTextElement.appendChild(editInput); // Append input field
 
-  editInput.focus(); // Fokus auf das Eingabefeld setzen
+  editInput.focus(); // Set focus to input field
 }
 
-// Hilfsfunktion, um die bearbeiteten Subtask-Änderungen zu speichern
+/**
+ * Saves the edited changes to a subtask.
+ * This function retrieves the edited subtask value from the input field,
+ * trims it, and updates the subtask text element if the value is not empty.
+ * @param {HTMLElement} subtaskTextElement - The element displaying the subtask text.
+ * @param {HTMLInputElement} editInput - The input field containing the edited subtask value.
+ * @returns {void}
+ */
 function saveEditedSubtask(subtaskTextElement, editInput) {
   let editedSubtask = editInput.value.trim();
   if (editedSubtask !== "") {
@@ -312,6 +383,13 @@ function saveEditedSubtask(subtaskTextElement, editInput) {
   }
 }
 
+/**
+ * Validates the form data and saves the task to JSON.
+ * This function retrieves form data, checks for required fields,
+ * prevents the default form submission behavior if required fields are missing,
+ * and then saves the task to JSON data if the form is valid.
+ * @returns {boolean} - Indicates whether the form is valid (false if validation fails).
+ */
 function validateForm() {
   let title = document.getElementById("title").value;
   let description = document.getElementById("description").value;
@@ -321,12 +399,18 @@ function validateForm() {
   let category = document.getElementById("category").value;
 
   if (title === "" || date === "" || category === "") {
-    return false; // Verhindert das Standardverhalten des Formulars
+    return false; // Prevents default form behavior
   }
   saveTaskToJson(title, description, date, prio, category, assignedTo);
   return false;
 }
 
+/**
+ * Initializes the JSON data for adding tasks.
+ * This function loads tasks data, converts it into an array of entries,
+ * and then adds the entries to the global allTasksJson array.
+ * @returns {Promise<void>}
+ */
 async function initJSONaddTasks() {
   let tasks = Object.entries(await loadData("tasks"));
   allTasksJson.push(tasks);
